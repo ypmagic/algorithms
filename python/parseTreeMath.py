cf99c435774defa0ff7b96f7de99a32289c35e3d
Young Park <young_park@brown.edu>
2018-08-25 17:29:40 -0700

    parse tree from binary tree

---
+++ python/parseTreeMath.py
@@ -0,0 +1,43 @@
+from pythonds.basic import Stack
+from pythonds.trees import BinaryTree
+import operator
+
+def buildParseTree(fpexp):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in '+-*/)':
+            currentTree.setRootVal(eval(i))
+            parent = pStack.pop()
+            currentTree = parent
+        elif i in '+-*/':
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+        elif i == ')':
+            currentTree = pStack.pop()
+        else:
+            raise ValueError("Unknown operator: " + i)
+    return eTree
+
+def evaluateParseTree(parseTree):
+    opers = {'+':operator.add, '-':operator.sub,
+            '*': operator.mul, '/':operator.truediv}
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC), evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+
+print(evaluateParseTree(buildParseTree("(3+(4*5))")))
